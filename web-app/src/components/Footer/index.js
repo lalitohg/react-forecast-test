60434081a31a973037347e6cd1f22bec77d9af88
lalitohg <eduardohdzgro@gmail.com>
2020-09-25 12:05:14 -0500

    Header and footer layout

--- web-app/src/components/Footer/index.js
+++ web-app/src/components/Footer/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import accuweatherlogo from './accuweatherlogo.png';
+import './index.css';
 
 export default function Footer() {
     return (
-        <div>footer</div>
+        <footer className="app-footer">
+            <Grid
+                container
+                direction="row"
+                >
+                <Grid item xs={6}>
+                    <a href="http://www.accuweather.com/">
+                        <img alt="" src={accuweatherlogo} width="24" height="24"></img>
+                    </a>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="caption" component="div" align="right">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></Typography>
+                </Grid>
+            </Grid>
+            
+        </footer>
     );
 }
--- web-app/src/components/Header/index.js
+++ web-app/src/components/Header/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Hidden from '@material-ui/core/Hidden';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import './index.css';
+import storm from './storm.svg';
 
 export default function Header() {
     return (
-        <div>header</div>
+        <header className="app-header">
+            <Grid
+                container
+                spacing={2}
+                direction="row"
+                justify="flex-start"
+                alignItems="center">
+                <Grid item>
+                    <a alt="AccuWeather" href="http://www.accuweather.com/"><img alt="" src={storm} width="24" height="24"></img></a>
+                </Grid>
+                <Grid item>
+                    <Hidden xsDown>
+                        <Typography>Forecasting Dummy App</Typography>
+                    </Hidden>
+                </Grid>
+            </Grid>
+        </header>
     );
 }
