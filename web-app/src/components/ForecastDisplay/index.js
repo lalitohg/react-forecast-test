104d11ee1cccbd9e76b10222bc2ab35008bec9e5
lalitohg <eduardohdzgro@gmail.com>
2020-09-26 01:15:05 -0500

    Implement reactive state in display component

--- web-app/src/components/ForecastDisplay/index.js
+++ web-app/src/components/ForecastDisplay/index.js
@@ -1,58 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import ForecastItem from '../ForecastItem';
+import loader from './loader.svg';
 import './index.css';
 
 export default function ForecastDisplay() {
+    const showLoader = useSelector(state => state.display.loading);
+    const forecastItems = useSelector(state => state.display.forecastItems);
     return (
         <Box className="forecast-display">
-            <Container>
-                <Grid container justify="space-between" spacing={2}>
-                    <Grid item>
-                        <ForecastItem
-                            date="Sep 25"
-                            min="40"
-                            max="40"
-                            dayIcon="1"
-                            nightIcon="38"
-                            unit='F'
-                        />
+            {showLoader && <Box style={{ textAlign: 'center' }}><img alt='' src={loader}></img></Box>}
+            {!forecastItems.length ?
+                <Box style={{ textAlign: 'center' }}><Typography variant="h2">No data to display</Typography></Box> :
+                <Container>
+                    <Grid container justify="space-between" spacing={2}>
+                        {forecastItems.map(item =>
+                            <Grid key={item.Date} item>
+                                <ForecastItem
+                                    date={new Date(item.Date).toString().substr(0, 10)}
+                                    min={item.Temperature.Minimum.Value}
+                                    max={item.Temperature.Maximum.Value}
+                                    dayIcon={item.Day.Icon}
+                                    nightIcon={item.Night.Icon}
+                                    unit={item.Temperature.Maximum.Unit}
+                                />
+                            </Grid>
+                        )}
                     </Grid>
-                    <Grid item>
-                        <ForecastItem
-                            date="Sep 25"
-                            min="40"
-                            max="40"
-                            dayIcon="1"
-                            nightIcon="38"
-                            unit='F'
-                        />
-                    </Grid>
-                    <Grid item>
-                        <ForecastItem
-                            date="Sep 25"
-                            min="40"
-                            max="40"
-                            dayIcon="1"
-                            nightIcon="38"
-                            unit='F'
-                        />
-                    </Grid>
-                    <Grid item>
-                        <ForecastItem
-                            date="Sep 25"
-                            min="40"
-                            max="40"
-                            dayIcon="1"
-                            nightIcon="38"
-                            unit='F'
-                        />
-                    </Grid>
-                </Grid>
-            </Container>
-
+                </Container>
+            }
         </Box>
     );
 }
